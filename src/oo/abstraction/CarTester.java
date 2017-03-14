1ac71c0fe8aa1a6e853a292703b2d36a81766eff
anny1002 <anny1002wade@gmail.com>
2017-03-14 15:56:43 +0800

    three cars

--- src/oo/abstraction/CarTester.java
+++ src/oo/abstraction/CarTester.java
@@ -4,12 +4,14 @@ public class CarTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Car c1 = new Car("toyota",1800,"Altis","sedan");
+		Car c1 = new Car("TOYOTA", 1800, "Altis", "sedan");
 		// c1.brand="toyota";
 		// c1.cc=1800;
 		// c1.name="Altis";
 		// c1.type="sedan";
-
+		Car c2 = new Car("HONDA",2000,"ACCORD","");
+		Car c3=new Car("TOYOTA",2000,"wish","");
+		
 	}
 
 }
