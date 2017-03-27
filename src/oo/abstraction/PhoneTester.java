704ac94d9244201ed5528ffd738a12e277789bac
anny1002 <anny1002wade@gmail.com>
2017-03-28 00:17:45 +0800

    phone(3)

--- src/oo/abstraction/PhoneTester.java
+++ src/oo/abstraction/PhoneTester.java
@@ -4,11 +4,24 @@ public class PhoneTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Phone p = new Phone(4.7f, "iphone",
+		Phone p1 = new Phone(4.7f, "Apple",
 				"12MP (million pixels) iSight shooter,3D touch technology,Live Photos feature",
 				"Rose Gold",
 				"iphone 6s");
-
+        Phone p2= new Phone(5.1f,"Samsung","","gold","galaxy s7");
+        Phone p3=new Phone(5.2f,"Htc","","red","htc 10");
+        Phone [] phone=new Phone[10];
+        phone[0]=new Phone(4.7f, "Apple",
+				"12MP (million pixels) iSight shooter,3D touch technology,Live Photos feature",
+				"Rose Gold",
+				"iphone 6s");
+        phone[1]=new Phone(5.1f,"Samsung","","gold","galaxy s7");
+        phone[2]=new Phone(5.2f,"Htc","","red","htc 10");
+        System.out.println(phone[0].color);
+        int i=0;
+        for (i=0;i<=2;i++){
+        	System.out.println(phone[i].size);
+        }
 	}
 
 }
