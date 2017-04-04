3581ba2b1f6257e011ac3ad89e39744620fb19c0
anny1002 <anny1002wade@gmail.com>
2017-04-05 01:15:17 +0800

    invoice

--- src/oo/test/Invoice.java
+++ src/oo/test/Invoice.java
@@ -1,10 +1,25 @@
 package oo.test;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Invoice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ArrayList<String> a = new ArrayList<>();
+		a.add("331");
+		a.add("821");
+		a.add("886");
+		a.add("554");
+		System.out.println("本次開獎號為");
+		System.out.println(a.get(0));
+		System.out.println(a.get(1));
+		System.out.println(a.get(2));
+		System.out.println(a.get(3));
+		Scanner scanner = new Scanner(System.in);
+		String x = scanner.nextLine();
+		
 	}
 
 }
