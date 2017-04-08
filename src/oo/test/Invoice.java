a3cf4017bc0d5736088e5cc53f0fdfe4deef1546
anny1002 <anny1002wade@gmail.com>
2017-04-08 13:44:30 +0800

    invoice

--- src/oo/test/Invoice.java
+++ src/oo/test/Invoice.java
@@ -7,20 +7,25 @@ public class Invoice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ArrayList<String> a = new ArrayList<>();
-		a.add("331");
-		a.add("821");
-		a.add("886");
-		a.add("554");
+		ArrayList<Integer> a = new ArrayList<>();
+		a.add(331);
+		a.add(821);
+		a.add(886);
+		a.add(554);
 		System.out.println("本次開獎號為");
 		System.out.println(a.get(0));
 		System.out.println(a.get(1));
 		System.out.println(a.get(2));
 		System.out.println(a.get(3));
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("請輸入您的發票號碼:");
 		String x = scanner.nextLine();
 		int correct=Integer.parseInt(x);
-		String x1 = (correct%1000);
+		int three=correct%1000;
+		if (three==a.get(0)||three==a.get(1)||three==a.get(2)||three==a.get(3)){
+			System.out.println("恭喜您中200元");
+		}
+		
 		
 	}
 
