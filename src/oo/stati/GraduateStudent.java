783b10f3f3a0ed5e1f1ae9f0c1cb9ae91ed8bc4e
user <user@es302n21>
2017-05-02 15:10:09 +0800

    push

--- src/oo/stati/GraduateStudent.java
+++ src/oo/stati/GraduateStudent.java
@@ -1,8 +1,8 @@
 package oo.stati;
 
-public class GraduateStudent {
+public class GraduateStudent extends Student{
 int thesis;
-public GraduateStudent (int english, int math, int chinese){
+public public GraduateStudent(int english, int math, int chinese) {
 	this.english=english;
 	this.math = math;
 	this.chinese = chinese;
--- src/oo/stati/Tester.java
+++ src/oo/stati/Tester.java
@@ -9,7 +9,7 @@ public class Tester {
 		stu1.print();
 		stu2.print();
 		//本來的pass是60，加上static後只要改一個全都會變
-		GraduateStudent gstu =new GraduateStudent(68,85,89);
+		GraduateStudent gstu =new GraduateStudent(68,77,88);
 		gstu.print();
 	}
 
